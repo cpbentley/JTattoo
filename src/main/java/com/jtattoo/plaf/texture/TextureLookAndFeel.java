d8bb2544be24a27278d031e8bb4569048733aa9c
Charles Bentley <mordan9@gmail.com>
2018-10-25 18:50:59 +0200

    on demand menu themes, looks and favorites
    A class LookFeelModule helps to manage the JMenu for installed look and feels.
    When root menu is activated by the user, existing look and feels populate the menu list. Those looks with themes have their own sub menu populated with their themes.
    Add current theme to favorites. Remove current theme from favorites

--- src/main/java/com/jtattoo/plaf/texture/TextureLookAndFeel.java
+++ src/main/java/com/jtattoo/plaf/texture/TextureLookAndFeel.java
@@ -480,4 +480,14 @@ public class TextureLookAndFeel extends AbstractLookAndFeel {
         table.put("SpinnerUI", BaseSpinnerUI.class.getName());
     }
 
+    @Override
+    public List getMyThemes() {
+       return themesList;
+    }
+
+    @Override
+    public void setMyTheme(String theme) {
+       setTheme(theme);
+    }
+
 } // end of class TextureLookAndFeel
